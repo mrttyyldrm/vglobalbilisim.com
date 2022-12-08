139e61deace6088f499bae42af4d7169614a0639
Mert Yıldırım <mrttyyldrm@gmail.com>
2022-12-09 01:42:37 +0300

    Explore Bölümü Eklendi

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $("section#home").load("home.html")
     $("section#products").load("products.html");
     $("section#show").load("show.html");
+    $("section#explore").load("explore.html");
     $("section#contact").load("contact.html");
     $("section#features").load("features.html");
     $("footer").load("footer.html");
